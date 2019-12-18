12e648f6f516213b9d4dc360b733979b548f9a05
tongyi <do1you@163.com>
2019-12-18 21:57:24 +0800

    初始化配置参数

--- src/modules/config/Module.php
+++ src/modules/config/Module.php
@@ -35,5 +35,38 @@ class Module extends \webadmin\Module
         
 		// 初始化模块
         \webadmin\modules\config\models\SysModules::initModule();
+
+		// 脚手架
+		if (!YII_ENV_PROD) {
+			Yii::$app->setModule('gii', [
+				'class' => 'yii\gii\Module',
+				'generators' => [
+					'model' => [
+						'class' => '\webadmin\generators\model\Generator',
+						'templates'=> [],
+					],
+					'crud' => [
+						'class' => '\webadmin\generators\crud\Generator',
+						'templates'=> [],
+					],
+					'module' => [
+						'class' => '\webadmin\generators\module\Generator',
+						'templates'=> [],
+					],
+				],
+			]);
+		}
+
+		// 用户组件
+		Yii::$app->setComponents([
+			'user' => [
+				'class' => '\yii\web\User',
+				'identityClass' => '\webadmin\modules\authority\models\AuthUser',
+				'enableAutoLogin' => true,
+				'enableSession' => true,
+				'loginUrl'=>['/authority/user/login'],
+			],
+		]);
+
     }
 }
